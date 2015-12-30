6139c0a4222574388464201afdc7c15943ae8563
Daniel Holderbaum <danielholderbaum@googlemail.com>
2015-12-30 12:54:50 +0100

    BlackHole now has gravity.

--- core/src/com/pixelscientists/gdx/jam/entity/BlackHole.java
+++ core/src/com/pixelscientists/gdx/jam/entity/BlackHole.java
@@ -1,13 +1,9 @@
 package com.pixelscientists.gdx.jam.entity;
 
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.pixelscientists.gdx.commons.physics.box2d.events.BodyActor;
+import com.badlogic.gdx.utils.Array;
+import com.pixelscientists.gdx.commons.physics.box2d.Box2DUtil;
 import com.pixelscientists.gdx.jam.di.DI;
 
 /**
@@ -16,18 +12,18 @@ import com.pixelscientists.gdx.jam.di.DI;
 public class BlackHole extends BodySpriteEntity {
     public BlackHole(Vector2 position) {
         super(BodyFactory.createBlackHole(3f, position), SpriteFactory.createBlackHole(3f));
+    }
 
-        BodyActor bodyActor = DI.getGameComponent().overlaySubSystem().add(body);
-        bodyActor.addListener(new InputListener() {
-            @Override
-            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-                System.out.print("ENTERED");
-            }
+    private Vector2 otherToHole = new Vector2();
 
-            @Override
-            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                System.out.print("EXITED");
-            }
-        });
+    @Override
+    public void update(float deltaTime) {
+        Array<Body> bodies = Box2DUtil.getBodiesInRadius(DI.getGameComponent().physicsSubSystem().world, body.getPosition(), 5f);
+        for (Body otherBody : bodies) {
+            otherToHole.set(body.getPosition()).sub(otherBody.getPosition());
+            float gravity = 1f / (otherToHole.len() + 0.1f);
+            otherBody.applyForceToCenter(otherToHole.scl(gravity), true);
+        }
+        super.update(deltaTime);
     }
 }
